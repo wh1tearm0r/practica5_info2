d759dc1a38a81a41da881b2a6d04e27cf659ab34
Jose Valverde <jval7331_@hotmail.com>
2025-11-09 19:41:30 -0500

    Practica 5

---
+++ practica5_parte1/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsEllipseItem>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QGraphicsScene * scene = new QGraphicsScene();
+    QGraphicsEllipseItem * particula = new QGraphicsEllipseItem();
+    particula->setRect(0, 0, 50, 50);
+    scene->addItem(particula);
+    QGraphicsView * view = new QGraphicsView(scene);
+    view->show();
+
+    return a.exec();
+}
